b8185df206c7e61a09a8598436d2cad167ce8775
bdinamo <briandinamo@hotmail.com>
2022-05-16 19:30:08 -0300

    Segundo commit

--- proyectotup/settings.py
+++ proyectotup/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from os import getenv
 from dotenv import load_dotenv
 
+load_dotenv(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
